6ee843b8b24a0a19ecf0954c6424ac171dcd595e
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-01-17 18:53:15 +0300

    save tablets to cookies after page mount

--- src/pages/Tablet/index.js
+++ src/pages/Tablet/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import cookies from 'js-cookie'
 
-export default function Tablet(props) {
+export default function Tablet({slug}) {
+  useEffect(() => {
+    const tabletsList = JSON.parse(cookies.get('tablets')) || []
+    if (!tabletsList.includes(slug)) {
+      tabletsList.push(slug)
+    }
+    cookies.set('tablets', tabletsList, {expires: +process.env.EXPIRE_PERIOD})
+  }, [])
+
   return (
     <div>
-      <h2>Страница планшета {props.slug}</h2>
+      <h2>Страница планшета {slug}</h2>
     </div>
   )
 }
