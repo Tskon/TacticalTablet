7b4bbc0c428dd80c48fedd0fa0be5529376f2c03
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-02-01 23:32:38 +0300

    change file ctructure

--- src/components/pixi/index.js
+++ src/components/pixi/index.js
@@ -1,69 +1,24 @@
 import React, {useEffect, useRef} from 'react'
 import * as PIXI from 'pixi.js'
+import app from '~/components/pixi/app'
+import IconFactory from '~/components/pixi/utils/IconFactory';
 import heavy from '~/common/images/wot/heavy.png'
 import art from '~/common/images/wot/art.png'
 import light from '~/common/images/wot/light.png'
 import medium from '~/common/images/wot/medium.png'
 
-const sprite = PIXI.Sprite.from
+const graphics = new PIXI.Graphics()
 
-const app = new PIXI.Application({
-  width: window.innerWidth - 10,
-  height: window.innerHeight - 150,
-  antialias: true,
-  transparent: false,
-  resolution: 1,
-  autoResize: true,
-  backgroundColor: 0x061639,
-})
-
-const dragNDrop = {
-  onDragStart(event) {
-    this.data = event.data
-    this.alpha = 0.5
-    this.dragging = true
-  },
-  onDragEnd() {
-    this.alpha = 1
-    this.dragging = false
-    this.data = null
-    // TODO save coords from this.x / this.y
-  },
-  onDragMove() {
-    if (this.dragging) {
-      const newPosition = this.data.getLocalPosition(this.parent)
-      this.x = newPosition.x
-      this.y = newPosition.y
-      // TODO periodically save coords from newPosition.x / newPosition.y
-    }
-  }
-}
-
-const addIcon = (img, x = 0, y = 0, color) => {
-  const icon = sprite(img)
-  icon.interactive = true
-  icon.buttonMode = true
-  icon.anchor.set(0.5, 0.5)
-  icon
-    .on('pointerdown', dragNDrop.onDragStart)
-    .on('pointerup', dragNDrop.onDragEnd)
-    .on('pointerupoutside', dragNDrop.onDragEnd)
-    .on('pointermove', dragNDrop.onDragMove)
-  icon.x = x
-  icon.y = y
-  if (color) icon.tint = color
-
-  app.stage.addChild(icon)
-}
+const iconFactory = new IconFactory(app)
 
 export default () => {
   const canvas = useRef()
 
   useEffect(() => {
-    addIcon(art, 100, 50, 0xFF0000)
-    addIcon(light, 200, 100, 0xFF0000)
-    addIcon(medium, 300, 300, 0x00FF00)
-    addIcon(heavy, 400, 50, 0x00FF00)
+    iconFactory.add(art, 100, 50, 0xFF0000)
+    iconFactory.add(light, 200, 100, 0xFF0000)
+    iconFactory.add(medium, 300, 300, 0x00FF00)
+    iconFactory.add(heavy, 400, 50, 0x00FF00)
 
     canvas.current.appendChild(app.view)
   }, [])
---
+++ src/components/pixi/utils/IconFactory.js
@@ -0,0 +1,46 @@
+import {Sprite} from 'pixi.js'
+const sprite = Sprite.from
+
+export default class IconFabric {
+  constructor(app) {
+    this.app = app
+  }
+
+  add(img, x = 0, y = 0, color) {
+    const icon = sprite(img)
+    icon.interactive = true
+    icon.buttonMode = true
+    icon.anchor.set(0.5, 0.5)
+    icon
+      .on('pointerdown', this.onDragStart)
+      .on('pointerup', this.onDragEnd)
+      .on('pointerupoutside', this.onDragEnd)
+      .on('pointermove', this.onDragMove)
+    icon.x = x
+    icon.y = y
+    if (color) icon.tint = color
+
+    this.app.stage.addChild(icon)
+  }
+
+  onDragStart(event) {
+    this.data = event.data
+    this.alpha = 0.5
+    this.dragging = true
+  }
+  onDragEnd() {
+    this.alpha = 1
+    this.dragging = false
+    this.data = null
+    // TODO save coords from this.x / this.y
+  }
+  onDragMove() {
+    if (this.dragging) {
+      const newPosition = this.data.getLocalPosition(this.parent)
+      this.x = newPosition.x
+      this.y = newPosition.y
+      // TODO periodically save coords from newPosition.x / newPosition.y
+    }
+  }
+
+}
