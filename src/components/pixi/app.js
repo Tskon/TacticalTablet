88723ba0d0890421d30f0d466a1f48f1fcbfdf7d
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-02-27 10:25:21 +0300

    fix canvas width

--- src/components/pixi/app.js
+++ src/components/pixi/app.js
@@ -1,8 +1,8 @@
 import {Application} from 'pixi.js'
 
 export default new Application({
-  width: window.innerWidth - 10,
-  height: window.innerHeight - 150,
+  width: document.body.clientWidth - 320,
+  height: document.body.clientHeight - 150,
   antialias: true,
   transparent: false,
   resolution: 1,
