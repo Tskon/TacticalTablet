292e87097093b5cab88a3a396da0b7a5a7a6684c
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-04-06 22:47:30 +0300

    send pointer data by ws

--- server/ws.js
+++ server/ws.js
@@ -3,13 +3,14 @@ import WebSocket from 'ws'
 const wss = new WebSocket.Server({port: 4321})
 
 wss.on('connection', (ws) => {
+  console.log('WS: connected')
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
 
     wss.clients.forEach((client) => {
-      console.log(client)
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        console.log(message)
+        client.sendString(message);
       }
     });
   });
