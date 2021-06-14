15e2e2fa2161856d20f3702175657e6c07708648
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-06-14 04:24:47 +0300

    link to main page in default template

--- src/templates/DefaultTemplate/index.js
+++ src/templates/DefaultTemplate/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import '~/common/styles/reset.scss'
 import styles from './index.scss'
+import {Link} from 'react-router-dom';
 
 export default function DefaultTemplate(props) {
   return (
     <div>
       <header className={styles.header}>
-        <h1 className={styles.title}>Тактический планшет</h1>
+        <Link to="/">
+          <h1 className={styles.title}>
+          Тактический планшет
+          </h1>
+        </Link>
       </header>
       {props.children}
     </div>
