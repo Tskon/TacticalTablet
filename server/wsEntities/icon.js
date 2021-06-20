f6320d9abd0b93d46dd71d66b0c252c53c7c2dc0
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-06-20 15:05:08 +0300

    add debounce for update function

--- server/wsEntities/icon.js
+++ server/wsEntities/icon.js
@@ -1,4 +1,4 @@
-import {iconActions} from '../../src/services/ws.js'
+import debounce from 'lodash/debounce.js'
 
 // TODO save icon positions to DB
 function createIcon(payload) {
@@ -8,14 +8,15 @@ function createIcon(payload) {
 function updateIcon(payload) {
   console.log('update', payload)
 }
+const debouncedUpdateIcon = debounce(updateIcon, 300)
 
 export const iconHandler = ({type, payload}) => {
   switch (type) {
-  case iconActions.create:
+  case 'create':
     createIcon(payload)
     break
-  case iconActions.update:
-    updateIcon(payload)
+  case 'update':
+    debouncedUpdateIcon(payload)
     break
   }
 }
