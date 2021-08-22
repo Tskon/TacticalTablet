4bf17e24de99c3138b197cdb5879bf19605183bf
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-08-22 23:45:29 +0300

    fix cookie ids content

--- src/pages/Tablet/index.js
+++ src/pages/Tablet/index.js
@@ -1,49 +1,37 @@
 import React, {useEffect, useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import Pixi from '~/components/pixi'
 import jsCookie from 'js-cookie'
 import {webSocket} from '~/services/clientWS'
 import copyToClipboard from '~/services/copyToClipboard'
-import {setList} from '~/store/tabletListSlice'
 import {setIconData} from '~/store/tabletDataSlice'
-import getTablets from '~/common/js/getTablets'
 
 function Tablet({slug}) {
   const dispatch = useDispatch()
-  const {list: tabletList} = useSelector(state => state.tabletList)
 
   const [tabletIds, setTabletIds] = useState({})
 
   useEffect(async () => {
     const {data, status} = await axios.get(`${process.env.API_URL}/tablet?id=${slug}`)
-
     if (status === 200) {
       const {icons, editId, viewId} = data
-
       setTabletIds({editId, viewId})
-
       if (icons && icons.length) {
         icons.forEach(icon => {
           dispatch(setIconData({data: icon}))
         })
       }
-
-      axios
-        .get( `${process.env.API_URL}/createWs`, {params: {tabletId: viewId}})
-        .then(webSocket.initWebSocket(viewId))
+      await axios.get( `${process.env.API_URL}/createWs`, {params: {tabletId: viewId}})
+      webSocket.initWebSocket(viewId)
     }
-  }, [])
 
-  useEffect(async () => {
-    if (!tabletList) {
-      dispatch(setList(await getTablets()))
-      return
-    }
-    const resultList = tabletList.includes(slug) ? tabletList : [...tabletList, slug]
+    const tabletsCookie = jsCookie.get('tablets')
+    const tablets = tabletsCookie ? JSON.parse(tabletsCookie) : []
+    const resultList = tablets.includes(slug) ? tablets : [...tablets, slug]
     jsCookie.set('tablets', resultList, {expires: +process.env.EXPIRE_PERIOD})
-  }, [tabletList])
+  }, [])
 
   const tabletShareLinks = Object.values(tabletIds).map(id => {
     const isEditId = /^edit-/.test(id)
