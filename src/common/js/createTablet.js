14327430f6d5ca25dfe91a3cbea1d3522d77fd2a
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-08-22 17:21:26 +0300

    add aspect ratio and title to payload for create tablet endpoint

--- src/common/js/createTablet.js
+++ src/common/js/createTablet.js
@@ -1,6 +1,9 @@
 import api from '~/common/js/api'
 
-export default async () => {
-  const {data} = await api.post('/tablet')
+export default async (payload = {
+  title: '',
+  aspectRatio: 0
+}) => {
+  const {data} = await api.post('/tablet', payload)
   return data
 }
--- src/pages/main/components/NewTablet/index.js
+++ src/pages/main/components/NewTablet/index.js
@@ -9,14 +9,8 @@ import CardActions from '@material-ui/core/CardActions'
 import Card from '@material-ui/core/Card'
 import {useHistory} from 'react-router-dom'
 
-
 export default function NewTablet() {
   const history = useHistory()
-
-  async function onCreateTablet() {
-    const {status, editId} = await createTablet()
-    if (status === 'ok') return history.push(`/tablet/${editId}`)
-  }
 
   const [title, setTitle] = useState('Тактический планшет')
   const titleHandler = (e) => {setTitle(e.target.value)}
@@ -27,55 +21,64 @@ export default function NewTablet() {
   const [aspectHeight, setAspectHeight] = useState(9)
   const aspectHeightHandler = (e) => {setAspectHeight(Number(e.target.value))}
 
+  async function onCreateTablet() {
+    const {status, editId} = await createTablet({
+      title,
+      aspectRatio: aspectWidth / aspectHeight
+    })
+    if (status === 'ok') return history.push(`/tablet/${editId}`)
+  }
 
   return (
-    <Card className={styles.newTablet}>
-      <CardHeader title="Новый планшет" />
+    <form onSubmit={onCreateTablet}>
+      <Card className={styles.newTablet}>
+        <CardHeader title="Новый планшет" />
 
-      <CardContent>
-        <TextField
-          className={styles.input}
-          fullWidth
-          label="Заголовок"
-          value={title}
-          onInput={titleHandler}
-        />
-        <div>
-          <div className={styles.aspectRatioTitle}>
-            Соотношение сторон:
+        <CardContent>
+          <TextField
+            className={styles.input}
+            fullWidth
+            label="Заголовок"
+            value={title}
+            onInput={titleHandler}
+          />
+          <div>
+            <div className={styles.aspectRatioTitle}>
+              Соотношение сторон:
+            </div>
+            <div className={styles.aspectRatio}>
+              <TextField
+                className={styles.input}
+                label="Ширина"
+                variant="outlined"
+                size="small"
+                type="number"
+                value={aspectWidth}
+                onInput={aspectWidthHandler}
+              />
+              <TextField
+                className={styles.input}
+                label="Высота"
+                variant="outlined"
+                size="small"
+                type="number"
+                value={aspectHeight}
+                onInput={aspectHeightHandler}
+              />
+            </div>
           </div>
-          <div className={styles.aspectRatio}>
-            <TextField
-              className={styles.input}
-              label="Ширина"
-              variant="outlined"
-              size="small"
-              type="number"
-              value={aspectWidth}
-              onInput={aspectWidthHandler}
-            />
-            <TextField
-              className={styles.input}
-              label="Высота"
-              variant="outlined"
-              size="small"
-              type="number"
-              value={aspectHeight}
-              onInput={aspectHeightHandler}
-            />
-          </div>
-        </div>
-      </CardContent>
+        </CardContent>
 
-      <CardActions>
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={onCreateTablet}
-        >
-          Создать планшет
-        </Button>
-      </CardActions>
-    </Card>
+        <CardActions>
+          <Button
+            color="primary"
+            variant="contained"
+            type="submit"
+          >
+            Создать планшет
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
   )
 }
