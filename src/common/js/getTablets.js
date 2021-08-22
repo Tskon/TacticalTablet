4f19a64e6bd411446397a1326e102d8d2fd0171c
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-08-22 23:49:46 +0300

    fix get tablets url

--- src/common/js/getTablets.js
+++ src/common/js/getTablets.js
@@ -5,6 +5,6 @@ export default async () => {
   const tabletsCookie = jsCookie.get('tablets')
   const tablets = tabletsCookie ? JSON.parse(tabletsCookie) : []
   if (!tablets.length) return
-  const {data} = await api.get(`${process.env.API_URL}/tablet-list?ids[]=${tablets.join('&ids[]=')}`)
+  const {data} = await api.get(`/tablet-list?ids[]=${tablets.join('&ids[]=')}`)
   return data
 }
