cd010df0c9e71dc16a8cf34cf859a0ab7b9bfced
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-08-22 13:33:04 +0300

    add form for new tablet

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,12 @@
 import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {useHistory, Link} from 'react-router-dom'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import createTablet from '~/common/js/createTablet'
 import {fetchListFromCookie} from '~/store/tabletListSlice'
 import styles from './index.scss'
@@ -21,23 +27,72 @@ export default function Main() {
 
   const tablets = list
     ? list.map(id => (
-      <Link
-        key={id}
-        to={`/tablet/${id}`}
-      >
-        {id}
-      </Link>
+      <li key={id}>
+        <Link to={`/tablet/${id}`}>
+          {id}
+        </Link>
+      </li>
     ))
     : []
 
   return (
-    <div>
-      <div className={styles.tabletsList}>
-        { tablets }
-      </div>
-      <button onClick={onCreateTablet}>
-        Создать планшет
-      </button>
+    <div className={styles.wrapper}>
+      <Card className={styles.tabletListWrapper}>
+        <CardHeader
+          title="Мои планшеты"
+          subheader="Ранее открытые планшеты"
+        />
+        <CardContent>
+          <ul className={styles.tabletList}>
+            { tablets }
+          </ul>
+        </CardContent>
+      </Card>
+
+      <Card className={styles.newTabletWrapper}>
+        <CardHeader
+          title="Новый планшет"
+        />
+
+        <CardContent>
+          <TextField
+            className={styles.newTabletInput}
+            fullWidth
+            label="Заголовок"
+          />
+          <div>
+            <div className={styles.newTabletAspectRatioTitle}>
+              Соотношение сторон:
+            </div>
+            <div className={styles.newTabletAspectRatio}>
+              <TextField
+                className={styles.newTabletInput}
+                label="Ширина"
+                variant="outlined"
+                size="small"
+                type="number"
+              />
+              <TextField
+                className={styles.newTabletInput}
+                label="Высота"
+                variant="outlined"
+                size="small"
+                type="number"
+              />
+            </div>
+          </div>
+
+        </CardContent>
+        <CardActions>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={onCreateTablet}
+          >
+            Создать планшет
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   )
 }
