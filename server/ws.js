c8760bb4b0e41e3431e067689c8d026eb942eecb
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2021-08-29 22:45:36 +0300

    fix multi sockets for one server

--- server/ws.js
+++ server/ws.js
@@ -1,12 +1,14 @@
+import {createServer} from 'http'
+import {parse} from 'url'
 import WebSocket from 'ws'
 import {iconHandler} from './wsEntities/icon.js'
 
+const httpServer = createServer()
 const serversList = {}
 
 function createWebSocketServer(wsId) {
   if (wsId in serversList) return
-
-  serversList[wsId] = new WebSocket.Server({port: 4321, path: `/${wsId}`})
+  serversList[wsId] = new WebSocket.Server({noServer: true})
   serversList[wsId].on('connection', (ws, req) => {
     console.log(`REQUEST URL: ${req.url}`, '\n', 'WS: connected')
     ws.on('message', function incoming(message) {
@@ -27,5 +29,21 @@ function createWebSocketServer(wsId) {
     })
   })
 }
+
+httpServer.on('upgrade', function upgrade(request, socket, head) {
+  const {pathname} = parse(request.url)
+
+  if (!pathname) {
+    socket.destroy()
+    return
+  }
+  createWebSocketServer(pathname)
+  const wss = serversList[pathname]
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit('connection', ws, request)
+  })
+})
+
+httpServer.listen(4321)
 
 export default createWebSocketServer
